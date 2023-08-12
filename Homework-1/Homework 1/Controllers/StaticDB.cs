d7680c3bcda2a7e3a16ce42561f6820230e005ae
Marko Gigoski <markogigoski@outlook.com>
2023-08-13 00:18:36 +0200

    Add files via upload

---
+++ Homework-1/Homework 1/Controllers/StaticDB.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Homework_1.Controllers
+{
+    public static class StaticDB
+    {
+        public static List<string> userNames = new List<string>
+        {
+            "Marko",
+            "Sharko",
+            "Darko",
+            "Tarko",
+            "Barko"
+        };
+    }
+}
---
+++ Homework-1/Homework 1/Controllers/Users.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Homework_1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Users : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<string>> Get()
+        {
+            var users = StaticDB.userNames;
+
+            return StatusCode(StatusCodes.Status200OK, users);
+        }
+
+        [HttpGet("index")]
+        public ActionResult<List<string>> GetByIndex(int index)
+        {
+            var users = StaticDB.userNames;
+
+            try
+            {
+                if (index < 0)
+                {
+                    return BadRequest("The value of the index canot be negative");
+                }
+                if (index >= users.Count)
+                {
+                    return NotFound($"The info on index-{index} does not exist");
+                }
+                return Ok(users[index]);
+
+            }catch(Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "An error ocurred, try again later");
+            }
+            
+        }
+    }
+}
